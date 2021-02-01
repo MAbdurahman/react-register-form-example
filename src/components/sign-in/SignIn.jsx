9dc3a12547dc2dd8bc6d898a2d87d9e5648e5d7c
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-01 04:12:00 -0600

    Complete temporary SignIn.jsx

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,5 +1,8 @@
 import { Avatar, Button, Grid, Paper, TextField, Typography, Link } from '@material-ui/core';
 import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import { AccountCircle, LockRounded } from '@material-ui/icons';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import React from 'react';
 import useStyles from './styles';
 
@@ -11,6 +14,57 @@ export default function SignIn() {
             <Grid className={classes.signin__heading} align='center' >
                <Avatar className={classes.signin__avatarStyle}><PersonOutlineIcon /></Avatar>
                <h2 className={classes.signin__headingTitle}>Sign In</h2>
+            </Grid>
+            <Grid className={classes.signin__textFieldContainer}>
+               <AccountCircle className={classes.signin__textFieldIcon} />
+               <TextField
+                  className={classes.signin__textFieldStyle}
+                  type='email'
+                  // id="input-with-icon-grid"
+                  label="Email"
+               />
+            </Grid>
+            <Grid className={classes.signin__textFieldContainer}>
+               <LockRounded className={classes.signin__textFieldIcon} />
+               <TextField
+                  className={classes.signin__textFieldStyle}
+                  type='password'
+                  // id="input-with-icon-grid"
+                  label="Password"
+               />
+            </Grid>
+            <Grid className={classes.signin__textFieldContainer}>
+               <FormControlLabel
+                  className={classes.signin__formControlStyle}
+                  control={
+                     <Checkbox
+                        name="checkedB"
+                        color="primary"
+                     />
+                  }
+                  label="Remember me"
+               />
+            </Grid>
+            <Button
+               className={classes.signin__buttonStyle}
+               type='submit'
+               fullWidth
+               variant='contained'
+            >
+               Sign In
+            </Button>
+            <Grid>
+               <Typography >
+                  <Link className={classes.linkStyle} href="#" >
+                     Forgot password?
+                  </Link>
+               </Typography>
+               <Typography>
+                  Do you have an account?&nbsp;&nbsp;
+                  <Link className={classes.linkStyle} href="#" >
+                     Sign Up
+                  </Link>
+               </Typography>
             </Grid>
          </Paper>
       </Grid>
