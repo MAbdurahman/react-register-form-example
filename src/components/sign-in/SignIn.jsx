542a8bd6572ab8eb132285a198cec14871ae64c1
MAbdurahman <mdbdrrhm@gmail.com>
2021-02-01 03:55:04 -0600

    Complete temporary styles.js for SignIn.jsx

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -1,11 +1,18 @@
+import { Avatar, Button, Grid, Paper, TextField, Typography, Link } from '@material-ui/core';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import React from 'react';
 import useStyles from './styles';
 
 export default function SignIn() {
    const classes = useStyles();
    return (
-      <div className={classes.signin}>
-         <h1>Sign In</h1>
-      </div>
+      <Grid>
+         <Paper className={classes.signin__paperStyle} elevation={10}>
+            <Grid className={classes.signin__heading} align='center' >
+               <Avatar className={classes.signin__avatarStyle}><PersonOutlineIcon /></Avatar>
+               <h2 className={classes.signin__headingTitle}>Sign In</h2>
+            </Grid>
+         </Paper>
+      </Grid>
    );
 };
--- src/components/sign-in/styles.js
+++ src/components/sign-in/styles.js
@@ -1,7 +1,79 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(theme => ({
-   signin: {
-      textAlign: 'center',
-   },
+	signin: {
+		marginTop: '1.125em',
+		textAlign: 'center',
+		fontSize: '.7rem',
+	},
+	signin__avatarStyle: {
+		marginBottom: '.5em',
+		marginTop: '.5em',
+		backgroundColor: '#423E37',
+	},
+	signin__buttonStyle: {
+		margin: '8px 0',
+		marginBottom: '1em',
+		fontSize: '1.3rem',
+		fontWeight: 500,
+		letterSpacing: '2px',
+		textTransform: 'capitalize',
+		backgroundColor: '#423E37',
+		color: '#EDEBD7',
+		'&:hover': {
+			backgroundColor: '#3F51B5',
+			color: '#FDEBD3',
+		},
+	},
+	signin__formControlStyle: {
+		margin: '0 auto',
+		marginLeft: '1.2em',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'flex-start',
+		[theme.breakpoints.down('xs')]: {
+			marginLeft: '.9em',
+		},
+	},
+	signin__heading: {
+		marginBottom: '2em',
+	},
+	signin__headingTitle: {
+		fontSize: '2rem',
+		fontWeight: 800,
+	},
+	signin__linkStyle: {
+		textDecoration: 'underline',
+		'&:hover': {
+			color: '#423E37',
+		},
+	},
+	signin__margin: {
+		margin: theme.spacing(1),
+		width: '100%',
+	},
+	signin__paperStyle: {
+		maxWidth: '420px',
+		margin: '30px auto',
+		padding: '20px',
+		maxHeight: '75vh',
+		height: '500px',
+		// [theme.breakpoints.between('md', 'lg')]: {
+		//    height: '55px',
+		// }
+	},
+	signin__textFieldContainer: {
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'nowrap',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	signin__textFieldIcon: {
+		marginRight: '5px',
+	},
+	signin__textFieldStyle: {
+		width: '80%',
+		marginBottom: '1em',
+	},
 }));
